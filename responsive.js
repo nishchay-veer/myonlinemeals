437f55342acbaa5950a0e3357664946ffbe16d7a
Nishchay Veer <nishchayveer19@gmail.com>
2022-02-27 20:58:59 +0530

    initial commit

---
+++ responsive.js
@@ -0,0 +1,31 @@
+// function phonenumber(inputtxt)
+// {
+//   var phoneno = /^\d{10}$/;
+//   if(inputtxt.value.match(phoneno))
+//   {
+//     return true;
+//   }
+//   else
+//   {
+//     alert("Not a valid Phone Number");
+//     return false;
+//   }
+// }
+// var form = document.getElementById('form')
+// var error = []
+// form.addEventListener('submit', function (event) {
+//     event.preventDefault()
+//     var username = document.getElementById('name').value
+//     // console.log(username)
+//     var phone = document.getElementById('phonenum').value
+//     // console.log(phone)
+
+//     var email = document.getElementById('Email').value
+//     // console.log(email)
+//     var message = document.getElementById('message').value
+//     if (username != "" && phone != "" && email != "" && message != "") {
+//         alert("Your form has been submitted successfully.")
+//     }
+//     // console.log(message)
+
+// })
